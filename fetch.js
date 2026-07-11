ec640e352e44961e8a13fd084724a68dda02a68c
roeno <36101221+roeno@users.noreply.github.com>
2026-07-11 15:10:27 +0200

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,14 +1,15 @@
+import axios from "axios";
 import fs from "fs";
 
 const apiKey = process.env.GOOGLE_API_KEY;
-const placeId = "ChIJ26WO-8_dQUcRtBFg3oBEkgU"; // ezt majd kicseréljük a sajátodra
+const placeId = "ChIJ26WO-8_dQUcRtBFg3oBEkgU";
 
 const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=reviews,rating,user_ratings_total&key=${apiKey}`;
 
 async function update() {
   try {
-    const res = await fetch(url);
-    const data = await res.json();
+    const res = await axios.get(url);
+    const data = res.data;
 
     if (!data.result || !data.result.reviews) {
       console.error("Nincs elérhető értékelés.");
@@ -23,4 +24,3 @@ async function update() {
 }
 
 update();
-
